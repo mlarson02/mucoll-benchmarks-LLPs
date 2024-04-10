Fixed d0/dz units. Improved randomisation

generation/pgun/pgun_lcio.py
@@ -46,7 +46,7 @@ for pdg in args.pdg:
 
 
 # Generating sampling distributions for each property (1 value/event)
-sample_size = args.events
+sample_size = args.events * args.particles
 rng = np.random.default_rng(12345)
 samples = {}
 configs = {
@@ -108,29 +108,30 @@ for e in range(args.events):
 	evt.setRunNumber(run.getRunNumber())
 	evt.addCollection(col, "MCParticle")
 	for p in range(args.particles):
+		s = e*args.particles + p
 		pdg_idx = p
 		if choose_random_pdg:
 			pdg_idx = np.random.choice(n_pdgs, 1)[0]
 		pdg = args.pdg[pdg_idx]
 		# Calculating all properties for this particle in the event
-		theta = samples['theta'][e]
-		phi = samples['phi'][e]
+		theta = samples['theta'][s]
+		phi = samples['phi'][s]
 		# Calculating momentum vector
 		if 'pt' in configs:
-			pt = samples['pt'][e]
+			pt = samples['pt'][s]
 			px = pt * math.cos(phi)
 			py = pt * math.sin(phi)
 			pz = pt / math.tan(theta)
 		elif 'p' in configs:
-			p = samples['p'][e]
+			p = samples['p'][s]
 			px = p * math.cos(phi) * math.sin(theta)
 			py = p * math.sin(phi) * math.sin(theta)
 			pz = p * math.cos(theta)
 		momentum = array('f', [px, py, pz])
-		# Calculating vertex position [converting mm -> cm] (LCIO default unit)
-		vx = samples['d0'][e] / 10.0 * math.cos(samples['dphi'][e])
-		vy = samples['d0'][e] / 10.0 * math.sin(samples['dphi'][e])
-		vz = samples['dz'][e] / 10.0
+		# Calculating vertex position
+		vx = samples['d0'][s] * math.cos(samples['dphi'][s])
+		vy = samples['d0'][s] * math.sin(samples['dphi'][s])
+		vz = samples['dz'][s]
 		vtx = array('d', [vx, vy, vz])
 		# Assigning properties to the MCParticle
 		mcp = IMPL.MCParticleImpl()